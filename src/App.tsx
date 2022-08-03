7d86f06ed055e632011e056ed74b0dd3e5ab52d6
Alexander Moreno <am@alexandermoreno.com>
2022-08-02 23:12:39 -0500

    threw initial values inside

--- src/App.tsx
+++ src/App.tsx
@@ -26,35 +26,39 @@ export default function App() {
     setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
   };
 
-  const initialNodes = [
-    {
-      id: '1',
-      type: 'input',
-      data: new Person('Stonebeard', ''),
-      position: { x: 250, y: 25 },
-    },
-    {
-      id: '2',
-      type: 'personNode',
-      data: (() => {
-        const mother = new Person('Stonebeard', 'Wife of Stonebeard');
-        mother.isMale = false;
-        return mother;
-      })(),
-      position: { x: 100, y: 125 },
-    },
-    {
-      id: '3',
-      type: 'personNode',
-      data: new Person('Stonebeard', 'First son of Stonebeard'),
-      position: { x: 250, y: 250 },
-    },
-  ];
 
-  const initialEdges = [
-    { id: 'f1-2', source: '1', target: '3' },
-    { id: 'm2-3', source: '2', target: '3', animated: true },
-  ];
+  const initialValues = useMemo(() => ({
+    edgeGraph: [
+      { id: 'f1-2', source: '1', target: '3' },
+      { id: 'm2-3', source: '2', target: '3', animated: true },
+    ],
+    nodeGraph: [
+      {
+        id: '1',
+        type: 'input',
+        data: new Person('Stonebeard', ''),
+        position: { x: 250, y: 25 },
+      },
+      {
+        id: '2',
+        type: 'personNode',
+        data: (() => {
+          const mother = new Person('Stonebeard', 'Wife of Stonebeard');
+          mother.isMale = false;
+          return mother;
+        })(),
+        position: { x: 100, y: 125 },
+      },
+      {
+        id: '3',
+        type: 'personNode',
+        data: new Person('Stonebeard', 'First son of Stonebeard'),
+        position: { x: 250, y: 250 },
+      },
+    ]
+  }), []);
+
+
 
   const nodeTypes = useMemo(() => ({ personNode: PersonNode }), []);
 
@@ -70,16 +74,17 @@ export default function App() {
           withNormalizeCSS
         >
           <AppLayout>
-            <PersonContext.Provider value={{edgeGraph: initialEdges, nodeGraph: initialNodes}}>
-            <Container
-              className="App"
-              style={{ height: '900px', width: '100%' }}
+            <PersonContext.Provider 
+              value={initialValues}
             >
-              <LineageGraph
-                nodeTypes={nodeTypes}
-              />
-            </Container>
-
+              <Container
+                className="App"
+                style={{ height: '900px', width: '100%' }}
+              >
+                <LineageGraph
+                  nodeTypes={nodeTypes}
+                />
+              </Container>
             </PersonContext.Provider>
           </AppLayout>
         </MantineProvider>
