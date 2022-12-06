bec0489150587d3e86dbbca1ba3f9ebf131f7bb6
MorenoAlexander <am@alexandermoreno.com>
2022-12-06 00:38:06 -0600

    converting to functional components; created zustand store

--- src/lib/models/Person.ts
+++ src/lib/models/Person.ts
@@ -1,44 +1,41 @@
+import crypto from 'crypto';
 import { Node } from 'react-flow-renderer';
 
-export default class Person {
+export interface Person {
+  id: string;
   firstName: string;
-
   lastName: string;
-
   fatherId?: string;
-
   motherId?: string;
-
-  isMale = true;
-
-  constructor(
-    firstName: string,
-    lastName: string,
-    fatherId?: string,
-    motherId?: string,
-  ) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-
-    if (fatherId) {
-      this.fatherId = fatherId;
-    }
-    if (motherId) {
-      this.motherId = motherId;
-    }
-  }
-
-  get label(): string {
-    return `${this.firstName} ${this.lastName}`;
-  }
-
-  get isRoot(): boolean {
-    return !this.fatherId && !this.motherId;
-  }
+  isMale: boolean;
 }
 
-export const NewMotherNode = (firstName: string, lastName: string) => {
-  const person = new Person(firstName, lastName);
-  person.isMale = false;
-  return person;
-};
+export const newOriginalPerson = (
+  firstName: string,
+  lastName: string,
+  isMale = true,
+  id?: string,
+): Person => ({
+  firstName,
+  lastName,
+  id: id || crypto.randomUUID(),
+  isMale: isMale || true,
+});
+
+export const generateOffspring = (
+  father: Person,
+  mother: Person,
+  firstName?: string,
+  lastName?: string,
+  isMale = true,
+): Person => ({
+  id: crypto.randomUUID(),
+  firstName: firstName || father.firstName,
+  lastName: father.lastName,
+  fatherId: father.id,
+  motherId: mother.id,
+  isMale,
+});
+
+export const personIsRoot = (person: Person): boolean =>
+  person.fatherId === undefined && person.motherId === undefined;
